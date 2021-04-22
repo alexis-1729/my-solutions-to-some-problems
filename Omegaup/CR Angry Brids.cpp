0f4f8c4eb7f31eb51f93c27d50f69b1c7b3abd8b
Alexis Hernández Ibañez <68455585+alexis-1729@users.noreply.github.com>
2021-04-22 12:49:24 -0500

    Create CR Angry Brids.cpp

---
+++ Omegaup/CR Angry Brids.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n,vx[25];int dp[17];
+int solve(int i){
+     if(tr>(3*60) || i>=n)return 0;
+     ans=dp[i];
+     if(ans!=-1)return ans;
+
+     else
+         ans=max()
+
+
+}
+main()
+{ios_base::sync_with_stdio(0);cin.tie(0);
+  cin>>n;
+  for(int i=0;i<n;i++)cin>>vx[i];
+
+    memset(dp,-1,sizeof(dp));
+  cout<<solve();
+}
