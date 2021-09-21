9e3c585af2e224ae59150b8bee93b5e83bc0759a
Alexis Hernández Ibañez <68455585+alexis-1729@users.noreply.github.com>
2021-09-21 14:08:09 -0500

    Create Cuantos Unos.cpp

---
+++ Omegaup/Cuantos Unos.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+const int maxn=1e5+1;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+   /* freopen("teamwork.in", "r", stdin);
+		freopen("teamwork.out", "w", stdout);*/
+  cin>>n;
+  cout<<__builtin_popcount(n);
+
+}
